4a521fbc2aa01fc42932a63810fbbb0d97108924
surajmudhole <svmdvg800@gmail.com>
2024-03-05 22:19:43 +0530

    Fixed the multer(upload file)bug.

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -7,16 +7,14 @@ export default class ProductController {
   }
   addProduct(req, res) {
     const { name, price, sizes } = req.body;
-    console.log(req.body, req.file.filename);
-
-    // const newProduct = {
-    //   name,
-    //   price: parseFloat(price),
-    //   sizes: sizes.split(","),
-    //   imageUrl: req.file.filename,
-    // };
-    // var rs = ProductModel.add(newProduct);
-    res.status(201).send("rs");
+    const newProduct = {
+      name,
+      price: parseFloat(price),
+      sizes: sizes.split(","),
+      imageUrl: req.file.filename,
+    };
+    var rs = ProductModel.add(newProduct);
+    res.status(201).send(newProduct);
   }
   getOneProduct(req, res) {}
   rateProduct(req, res) {}
--- src/middlewares/fileupload.middleware.js
+++ src/middlewares/fileupload.middleware.js
@@ -2,13 +2,13 @@
 import multer from "multer";
 
 // 2. Configure storage with filename and location.
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./uploads");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + file.originalname);
+    const uniqueSuffix = Date.now();
+    cb(null, uniqueSuffix + "-" + file.originalname);
   },
 });
 
