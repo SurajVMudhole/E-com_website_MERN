f511261389207913a609f5bba4d29d1d0ac64058
surajmudhole <svmdvg800@gmail.com>
2024-03-10 09:53:57 +0530

    Added basic authentiaction process

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 //import all the costom libraries
 import ProductRouter from "./src/features/product/product.routers.js";
 import userRoutes from "./src/features/user/user.routes.js";
+import basicAuthorizer from "./src/middlewares/basicAuth.middleware.js";
 //------------------------------------------------------------------------------
 // create consts
 const server = express();
@@ -13,22 +14,22 @@ const port = 8080;
 
 //Redirect API paths
 server.use(bodyParser.json());
-server.use("/api/products", ProductRouter);
+server.use("/api/products", basicAuthorizer, ProductRouter);
 server.use("/api/Users", userRoutes);
 //------------------------------------------------------------------------------
 
 // Router paths defined here
 server.get("/", (req, res) => {
-    res.status(200).send("Welcome to E-commerce Webservice!");
+  res.status(200).send("Welcome to E-commerce Webservice!");
 });
 //------------------------------------------------------------------------------
 
 //Start the server
 server.listen(port, (err) => {
-    if (err) console.error(`Failed to start server: ${err}`);
-    else {
-        console.log(`Server is Up on port ${port}`);
-        console.log(`http://localhost:${port}`);
-    }
+  if (err) console.error(`Failed to start server: ${err}`);
+  else {
+    console.log(`Server is Up on port ${port}`);
+    console.log(`http://localhost:${port}`);
+  }
 });
 //------------------------------------------------------------------------------
--- src/features/user/user.model.js
+++ src/features/user/user.model.js
@@ -1,24 +1,29 @@
 export default class UserModel {
-    constructor(name, email, password, type) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.type = type;
-    }
-    static signup(name, email, password, type) {
-        const newUser = new UserModel(name, email, password, type);
-        Users.push(newUser);
-    }
-    static signin(email, password) {
-        const result = Users.find((u) => u.email == email);
-        if (result.password === password) return result;
-        else return false;
-    }
+  constructor(name, email, password, type) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.type = type;
+  }
+  static signup(name, email, password, type) {
+    const newUser = new UserModel(name, email, password, type);
+    Users.push(newUser);
+  }
+  static signin(email, password) {
+    const result = Users.find((u) => u.email == email);
+    if (result.password === password) return result;
+    else return false;
+  }
+  static getAll() {
+    return Users;
+  }
 }
 
-var Users = [{
+let Users = [
+  {
     name: "Seller User",
     email: "seller@ecom.com",
     password: "seller@12345",
     type: "seller",
-}, ];
+  },
+];
