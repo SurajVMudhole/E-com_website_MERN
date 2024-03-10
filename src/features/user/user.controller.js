43a533a7d0c131533771da16908ed1dbad80572c
surajmudhole <svmdvg800@gmail.com>
2024-03-10 11:24:31 +0530

    added jwt but failed

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,21 +1,35 @@
 import UserModel from "./user.model.js";
+import jwt from "jsonwebtoken";
 export default class UserController {
-    signUp(req, res) {
-        UserModel.signup(
-            req.body.name,
-            req.body.email,
-            req.body.password,
-            req.body.type
-        );
-        res
-            .status(201)
-            .send(
-                `User Created.\n Name: ${req.body.name}\n Email: ${req.body.email}`
-            );
+  signUp(req, res) {
+    UserModel.signup(
+      req.body.name,
+      req.body.email,
+      req.body.password,
+      req.body.type
+    );
+    res
+      .status(201)
+      .send(
+        `User Created.\n Name: ${req.body.name}\n Email: ${req.body.email}`
+      );
+  }
+  signIn(req, res) {
+    const result = UserModel.signin(req.body.email, req.body.password);
+    console.log(result);
+    if (!result) {
+      res.status(400).send("User login Failed!");
+    } else {
+      const token = new jwt.sign(
+        { email: result.email },
+        "HlpNljWaP1EFVoWpKWLoSAYGnNdC7mYc",
+        {
+          expiresIn: "1h",
+        }
+      );
+      //2.send token
+      console.log(token);
+      return res.status(200).send(token);
     }
-    signIn(req, res) {
-        const result = UserModel.signin(req.body.email, req.body.password);
-        if (!result) res.status(400).send("User login Failed!");
-        else res.status(200).send("User login successful    ");
-    }
+  }
 }
--- src/middlewares/basicAuth.middleware.js
+++ src/middlewares/basicAuth.middleware.js
@@ -1,14 +1,15 @@
 import UserModel from "../features/user/user.model.js";
 const basicAuthorizer = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.status(401).send("Unauthorized");
-  const base64Credentials = authHeader.replace("Basic", "");
-  const decodeCreds = Buffer.from(base64Credentials, "base64").toString("utf8");
-  const Creds = decodeCreds.split(":");
-  const user = UserModel.getAll().find(
-    (u) => u.email == Creds[0] && u.password == Creds[1]
-  );
-  if (!user) return res.status(401).send("Invalid username or password");
-  else next();
+    const authHeader = req.headers["authorization"];
+    if (!authHeader)
+        return res.status(401).send("Unauthorized, No User Credentials found!!  ");
+    const base64Credentials = authHeader.replace("Basic", "");
+    const decodeCreds = Buffer.from(base64Credentials, "base64").toString("utf8");
+    const Creds = decodeCreds.split(":");
+    const user = UserModel.getAll().find(
+        (u) => u.email == Creds[0] && u.password == Creds[1]
+    );
+    if (!user) return res.status(401).send("Invalid username or password");
+    else next();
 };
 export default basicAuthorizer;
