8a150d472d7bd7bdf0fd8076bb399d4ef2866611
surajmudhole <svmdvg800@gmail.com>
2024-03-21 17:51:11 +0530

    addded wiston logger.

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,19 +1,35 @@
-import fs from "fs";
+// import fs from "fs";
 
-const fsPromise = fs.promises;
-async function log(logData) {
-    try {
-        const uplogData = `${new Date().toString()}.Log Data:${logData}\n`;
-        await fsPromise.appendFile("log.txt", uplogData);
-    } catch (err) {
-        console.log(err);
-    }
-}
+// const fsPromise = fs.promises;
+// async function log(logData) {
+//     try {
+//         const uplogData = `${new Date().toString()}.Log Data:${logData}\n`;
+//         await fsPromise.appendFile("log.txt", uplogData);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
 
+// const loggerMiddleware = async(req, res, next) => {
+//     if (!req.url.includes("signin")) {
+//         const data = `${req.url}-${JSON.stringify(req.body)}`;
+//         await log(data);
+//     }
+//     next();
+// };
+
+// export default loggerMiddleware;
+
+import winston from "winston";
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.json(),
+    transports: [new winston.transports.File({ filename: "log.txt" })],
+});
 const loggerMiddleware = async(req, res, next) => {
     if (!req.url.includes("signin")) {
         const data = `${req.url}-${JSON.stringify(req.body)}`;
-        await log(data);
+        logger.info(data);
     }
     next();
 };
